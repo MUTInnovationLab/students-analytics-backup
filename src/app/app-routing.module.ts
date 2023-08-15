16af19e769535afeca277b700c799c7e11e24364
vusumuzi gwala <vgwala149@gmail.com>
2023-08-15 15:57:35 +0200

    ll

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,86 +1,3 @@
-<<<<<<< HEAD
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  // {
-  //   path: 'home',
-  //   loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  // },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
-  {
-    path: 'profile',
-    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
-  },
-  {
-    path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
-  },
-  {
-    path: 'dashboard',
-    loadChildren: () => import('./FacultyMember/dashboard/dashboard.module').then( m => m.DashboardPageModule)
-  },
-  {
-    path: 'add-student',
-    loadChildren: () => import('./FacultyMember/add-student/add-student.module').then( m => m.AddStudentPageModule)
-  },
-  {
-    path: 'upload-spreadsheet',
-    loadChildren: () => import('./FacultyMember/upload-spreadsheet/upload-spreadsheet.module').then( m => m.UploadSpreadsheetPageModule)
-  },
-  {
-    path: 'fm-profile',
-    loadChildren: () => import('./fm-profile/fm-profile.module').then( m => m.FMProfilePageModule)
-  },
-  {
-    path: 'add-mentor',
-    loadChildren: () => import('./add-mentor/add-mentor.module').then( m => m.AddMentorPageModule)
-  },
-
-  {
-    path: 'admin-dashboard',
-    loadChildren: () => import('./admin-dashboard/admin-dashboard.module').then( m => m.AdminDashboardPageModule)
-  },
-  {
-    path: 'add-member',
-    loadChildren: () => import('./add-member/add-member.module').then( m => m.AddMemberPageModule)
-  },
-  {
-    path: 'member',
-    loadChildren: () => import('./member/member.module').then( m => m.MemberPageModule)
-  },
-  {
-    path: 'my-modal',
-    loadChildren: () => import('./my-modal/my-modal.module').then( m => m.MyModalPageModule)
-  },
-  
-  {
-    path: 'add-m-modal',
-    loadChildren: () => import('./add-m-modal/add-m-modal.module').then( m => m.AddMModalPageModule)
-  },
-
-  
-
-
-
-];
-
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
-=======
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -140,10 +57,7 @@ const routes: Routes = [
     path: 'my-modal',
     loadChildren: () => import('./my-modal/my-modal.module').then( m => m.MyModalPageModule)
   },
-  {
-    path: 'add-lecture',
-    loadChildren: () => import('./add-lecture/add-lecture.module').then( m => m.AddLecturePageModule)
-  },
+  
   {
     path: 'add-m-modal',
     loadChildren: () => import('./add-m-modal/add-m-modal.module').then( m => m.AddMModalPageModule)
@@ -156,7 +70,8 @@ const routes: Routes = [
   {
     path: 'courses',
     loadChildren: () => import('./courses/courses.module').then( m => m.CoursesPageModule)
-  },  {
+  },
+  {
     path: 'modules',
     loadChildren: () => import('./modules/modules.module').then( m => m.ModulesPageModule)
   }
@@ -174,4 +89,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
->>>>>>> 5f018f8c01514a120902d01826fe9663a4b7a464
