ee3114c361e7a3085a3a28c44962a60dde5fdc42
vusumuzi <vgwala149@gmail.com>
2023-08-21 14:04:30 +0200

    register user

---
+++ src/app/shared/register.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  constructor(
+    private auth: AngularFireAuth,
+    private firestore: AngularFirestore
+  ) { }
+
+  register(member: any) {
+    return this.auth.createUserWithEmailAndPassword(member.email, member.staffNumber)
+      .then(async authResult => {
+        const user = this.auth.currentUser;
+      if (await user) {
+        return this.firestore.collection('registered').doc(member.email).set(member);
+      }
+      })
+      .catch(error => {
+        // Handle error here
+        console.error('Registration error:', error.message);
+        throw error;
+      });
+  }
+}
